148f8bed926c606ce45f57b224f559ec8d6c40fc
scott <scott.fanatic@gmail.com>
2015-11-11 09:37:46 +0800

    Update const.go

--- Practice/const.go
+++ Practice/const.go
@@ -15,3 +15,36 @@ const (
 // 高精度:
 const Ln2= 0.693147180559945309417232121458176568075500134360255254120680009
 const Log2E= 1/Ln2 // 高精度 // HL
+
+
+//（ 简单地讲，每遇到一次 const 关键字，iota 就重置为 0 ）。
+//与c 的enum一样（未定义值，则默认从0开始）
+
+//当然，常量之所以为常量就是恒定不变的量，因此我们无法在程序运行过程中修改它的值；如果你在代码中试图修改常量的值则会引发编译错误。
+
+//引用 time 包中的一段代码作为示例：一周中每天的名称。
+
+const (
+    Sunday = iota
+    Monday
+    Tuesday
+    Wednesday
+    Thursday
+    Friday
+    Saturday
+)
+//你也可以使用某个类型作为枚举常量的类型：
+
+type Color int
+const (
+    RED Color = iota // 0
+    ORANGE // 1
+    YELLOW // 2
+    GREEN // ..
+    BLUE
+    INDIGO
+    VIOLET // 6
+)
+
+
+
